bf4cab66ef4ebfe77f2f9527bb60c6c65985768b
Brett Witherspoon <brett@witherspoon.engineering>
2024-07-07 18:34:05 -0400

    mpfs-disco-kit: Remove download of unused cores

--- sources/mpfs-disco-kit/components.tcl
+++ sources/mpfs-disco-kit/components.tcl
@@ -8,11 +8,6 @@ download_core -vlnv {Actel:SgCore:PF_DRI:*} -location {www.microchip-ip.com/repo
 download_core -vlnv {Actel:DirectCore:CoreAPB3:4.2.100} -location {www.microchip-ip.com/repositories/DirectCore}
 download_core -vlnv {Actel:DirectCore:COREAXI4DMACONTROLLER:2.1.102} -location {www.microchip-ip.com/repositories/DirectCore}
 download_core -vlnv {Actel:DirectCore:CoreGPIO:3.2.102} -location {www.microchip-ip.com/repositories/DirectCore}
-download_core -vlnv {Actel:SystemBuilder:PF_SRAM_AHBL_AXI:*} -location {www.microchip-ip.com/repositories/SgCore}
-download_core -vlnv {Actel:DirectCore:corepwm:4.5.100} -location {www.microchip-ip.com/repositories/DirectCore}
-download_core -vlnv {Actel:DirectCore:COREI2C:7.2.101} -location {www.microchip-ip.com/repositories/DirectCore}
-download_core -vlnv {Actel:DirectCore:CORESPI:5.2.104} -location {www.microchip-ip.com/repositories/SgCore}
-download_core -vlnv {Actel:DirectCore:CoreUARTapb:5.7.100} -location {www.microchip-ip.com/repositories/DirectCore}
 
 smartdesign \
     -memory_map_drc_change_error_to_warning 1 \

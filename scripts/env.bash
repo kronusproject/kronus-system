3f42a728caa0a1d138e14da458bf8d12f4cfe5f1
Brett Witherspoon <brett@witherspoon.engineering>
2024-10-06 10:00:38 -0400

    scripts: Update environment script

--- scripts/env.bash
+++ scripts/env.bash
@@ -1,7 +1,52 @@
-# Microchip
-MICROCHIP_INSTALL_DIR=${MICROCHIP_INSTALL_DIR:=/opt/Microchip}
-export SC_INSTALL_DIR=$MICROCHIP_INSTALL_DIR/SoftConsole-v2022.2-RISC-V-747
-export LIBERO_INSTALL_DIR=$MICROCHIP_INSTALL_DIR/Libero_SoC_v2024.1
+
+#!/usr/bin/env bash
+
+RED='\033[0;31m'
+NC='\033[0m'
+
+
+usage() {
+    echo "Set MICROCHIP_INSTALL_DIR environment variable or copy this file to the root if the install location"
+    echo
+    echo "usage: source ${BASH_SOURCE[0]}"
+    echo
+}
+
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+    echo -e "${RED}This script should be sourced not executed${NC}" 1>&2
+    usage
+    exit 1
+fi
+
+# Install paths
+SCRIPT_DIR=$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")
+MICROCHIP_INSTALL_DIR=${SCRIPT_DIR}
+LIBERO_INSTALL_DIR=$MICROCHIP_INSTALL_DIR/Libero_SoC_v2024.1
+SC_INSTALL_DIR=$MICROCHIP_INSTALL_DIR/SoftConsole-v2022.2-RISC-V-747
+
+if [[ ! -d ${MICROCHIP_INSTALL_DIR} ]]; then
+  echo -e "${RED}Libero install directory does not exist: ${MICROCHIP_INSTALL_DIR}${NC}" 1>&2
+  usage
+  return 1
+fi
+
+if [[ ! -d ${LIBERO_INSTALL_DIR} ]]; then
+  echo -e "${RED}Libero install directory does not exist: ${LIBERO_INSTALL_DIR}${NC}" 1>&2
+  usage
+  return 1
+fi
+
+if [[ ! -d ${SC_INSTALL_DIR} ]]; then
+  echo -e "${RED}Libero install directory does not exist: ${SC_INSTALL_DIR}${NC}" 1>&2
+  usage
+  return 1
+fi
+
+echo "Libero install: ${LIBERO_INSTALL_DIR}"
+echo "SoftConsole install: ${SC_INSTALL_DIR}"
+
+export SC_INSTALL_DIR
+export LIBERO_INSTALL_DIR
 
 # SoftConsole
 export PATH=$PATH:$SC_INSTALL_DIR/riscv-unknown-elf-gcc/bin
@@ -10,7 +55,7 @@ export FPGENPROG=$LIBERO_INSTALL_DIR/Libero/bin64/fpgenprog
 # Libero
 export PATH=$PATH:$LIBERO_INSTALL_DIR/Libero/bin:$LIBERO_INSTALL_DIR/Libero/bin64
 export PATH=$PATH:$LIBERO_INSTALL_DIR/Synplify/bin
-export PATH=$PATH:$LIBERO_INSTALL_DIR/Model/modeltech/linuxacoem
+export PATH=$PATH:$LIBERO_INSTALL_DIR/QuestaSim/bin
 
 # License
 export LM_LICENSE_FILE=1702@${LICENSE_SERVER:=localhost}
